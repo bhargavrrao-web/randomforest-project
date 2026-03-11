c8a4d4ffe2092dc07b3b5fee8e7489ca58f475bf
bhargavrrao@gmail.com <your_email@gmail.com>
2026-03-11 16:08:21 +0530

    updated code

--- randomforest.py
+++ randomforest.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
-X = [[10],[20],[30],[40],[50]]
-y = [15,25,35,45,55]
+X = [[10],[20],[30],[40],[50],[75],[41]]
+y = [15,25,35,45,55,11,13]
 
 model = RandomForestRegressor()
 model.fit(X,y)
