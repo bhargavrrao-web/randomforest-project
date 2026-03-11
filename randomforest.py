1a9b58c886c866a28ce24964ff1b78b1b6873513
bhargavrrao@gmail.com <your_email@gmail.com>
2026-03-11 11:14:55 +0530

    added random forest code

---
+++ randomforest.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+X = [[10],[20],[30],[40],[50]]
+y = [15,25,35,45,55]
+
+model = RandomForestRegressor()
+model.fit(X,y)
+
+X_marks = [[70]]
+print(model.predict(X_marks))
